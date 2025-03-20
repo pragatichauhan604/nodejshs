d9ce0332d5e986472011a10a7ccb62e7a49eb6c6
pragati <pragatichauhan04@gmail.com>
2025-03-20 12:42:05 +0530

    basic changes

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-require('dotenv').config()
-const express =require('express')
-const app =express()
-// const port =3000
-app.get('/',(req,res)=>{
-    res.send('hello world')
-}
-)
-app.get('/twitter',(req,res)=>{
-    res.send('hii this pragati')
-})
-app.get('/login',(req,res)=>{
-    res.send('<h1>please login at chai aur code</h1>')
-})
-app.get('/youtube',(res,req)=>{
-    res.send('')
-})
-app.listen(process.env.PORT,()=>{
-    console.log(`Example app listening on port ${process.env.PORT}`);
+// require('dotenv').config()
+// const express =require('express')
+// const app =express()
+// // const port =3000
+// const data= {name:'raman',desig:'sd'}
+// app.get('/',(req,res)=>{
+//     res.send('hello world')
+// }
+// )
+// app.get('/twitter',(req,res)=>{
+//     res.send('hii this pragati')
+// })
+// app.get('/login',(req,res)=>{
+//     res.send('<h1>please login at chai aur code</h1>')
+// })
+// app.get('/youtube',(req,res)=>{
+//     res.send('')
     
-})
+// })
+// app.get('/github',(req,res)=>{
+//     res.json(data)
+// })
+// app.listen(process.env.PORT,()=>{
+//     console.log(`Example app listening on port ${process.env.PORT}`);
+    
+// })
