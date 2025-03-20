031f9eff4bf9350c9742544f1e29eea9e88ca1f2
pragati <pragatichauhan04@gmail.com>
2025-03-21 02:37:04 +0530

    custom_api response and error handling

---
+++ src/utils/asynchandler.js
@@ -0,0 +1,31 @@
+    const asyncHandler = (requestHandler)=>{
+        (req, res, next)=>{
+            Promise.resolve(requestHandler(req,res,next))
+            .catch((err)=>next(err))
+        }
+
+    }
+    
+    export {asyncHandler}
+
+
+
+
+    // const asyncHandler=(fn)=>{}
+    // const asyncHandler=(fn)=> ()=>{}
+    // const asyncHandler=(fn)=> async()=>{}
+   
+   
+   
+        // const asyncHandler=(fn)=> async(req,res,next)=>{
+    // try {
+    //     await fn(req, res, next )
+    // } catch (error) {
+    //     console.error("");
+    //     res.status(error.code || 500).json({
+    //         success:false,
+    //         message:error.message
+    //     })
+        
+    // }
+    //     }
