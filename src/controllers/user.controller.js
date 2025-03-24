0c09f46e43cffa4a67a8b97ab94e8dd2473c3e1a
pragati <pragatichauhan04@gmail.com>
2025-03-24 23:53:34 +0530

    all set up configured

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,11 @@
+import { asyncHandler } from "../utils/asynchandler.js";
+
+
+const registerUser = asyncHandler( async (req,res)=>
+{
+  res.status(200).json({
+        message:"hello pragati welcome to my club"
+    })
+})
+
+export {registerUser}
