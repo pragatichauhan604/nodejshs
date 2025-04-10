9ddb341a88068db8b5aa84b6c4ec3943e78f94c1
pragati <pragatichauhan04@gmail.com>
2025-04-10 17:57:13 +0530

    addrefresh token end point

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -182,6 +182,43 @@ const logoutUser = asyncHandler(async(req, res) => {
     .clearCookie("refreshToken", options)
     .json(new ApiResponse(200, {}, "User logged Out"))
 })
+ const refreshAccessToken = asyncHandler(async(req,res)=>{
+   try {
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+    if(!refreshToken){
+     throw new ApiError(401,"unautharize request")
+    } 
+    const decodedToken = jwt.verify(
+     incomingRefreshToken,
+     process.env.REFRESH_TOKEN_SECRET
+    )
+    const user = await User.findById(decodedToken?._id)
+    if(!user){
+     throw new ApiError(401,"Invalid Refresh Token")
+    
+    }
+    if(incomingRefreshToken !== user.refreshToken){
+     throw new ApiError(401,"refresh token is expired or used")
+   }
+   const options={
+     httpOnly:true,
+     secure:true
+   }
+   const {accessToken,newRefreshToken}=await generateAccessAndRefereshTokens(user._id)
+   return res.status(200)
+   .cookie("accessToken",accessToken,options)
+   .cookie("newRefreshToken",newRefreshToken.options)
+   .json(
+     new ApiResponse(200,{accessToken,refreshToken:newRefreshToken},"Accsess Token refreshed successfully")
+   )
+   } catch (error) {
+    throw new ApiError(401,error?.message||"Invalid refresh token");
+    
+   }
+
+}
+
+)
 
 
 
@@ -192,6 +229,7 @@ export {
     registerUser,
     loginUser,
     logoutUser,
+    refreshAccessToken
   
    
 }
